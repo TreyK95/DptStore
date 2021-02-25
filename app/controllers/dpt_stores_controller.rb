ea656cdcf5f97a1545738f6412af0654b80fcb46
Trey Kemp <kemptrey@outlook.com>
2021-02-24 19:09:18 -0700

    working on index

--- app/controllers/dpt_stores_controller.rb
+++ app/controllers/dpt_stores_controller.rb
@@ -1,6 +1,10 @@
 class DptStoresController < ApplicationController
-
     def index
-        render component: "Dptstores"
+      @dpt_stores= DptStore.all 
+        render component: "Dptstores", props: {dpt_stores: @dpt_stores}
+    end
+
+    def new
+        render json: {dptstore: DptStore.all}
     end
 end
--- app/javascript/components/Dptstores.js
+++ app/javascript/components/Dptstores.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-export default () => {
-
+export default (props) => {
+    const {dpt_stores} = props
     return (
       <>
          <h1>Dptstore Component Here</h1>
+         {dpt_stores.length}
       </>
     );
-
 }
